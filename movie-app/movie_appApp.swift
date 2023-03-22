172898bcd02b6a8a5b8ffa8f241559087286a07a
Yean SEYHA <yeanseyha7@gmail.com>
2023-03-22 17:38:31 +0700

    Initial Commit

---
+++ movie-app/movie_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_appApp.swift
+//  movie-app
+//
+//  Created by Yean Seyha on 22/3/23.
+//
+
+import SwiftUI
+
+@main
+struct movie_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
